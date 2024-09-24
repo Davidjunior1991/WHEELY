99d051ec06fc9b19c0e2469f3239a2a82a6a3bfe
Ndivhuwo Khethani <ndivhuwokhethani8@gmail.com>
2024-09-24 14:53:23 +0200

    pulling

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_24_092124) do
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.string "date"
+    t.date "date"
     t.integer "duration"
     t.integer "status"
     t.bigint "user_id", null: false
