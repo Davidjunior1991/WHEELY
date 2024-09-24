ae936bc3d40128f3615a0a78791780616031bdf8
Ndivhuwo Khethani <ndivhuwokhethani8@gmail.com>
2024-09-24 14:32:35 +0200

    pulling

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,8 @@
 class BookingsController < ApplicationController
     def index
-      @user_bookings = Booking.where(user: current_user) 
-      @user_cars = Car.where(user: current_user) 
+      raise
+      @user_bookings = Booking.where(car.user = current_user) 
+      @user_cars = Car.where(user = current_user) 
      end
 
   def new
